919764a15a0b56c34caf72b8eba054f2461ed2ea
Joseph-MHY <huamaniyallij@gmail.com>
2024-02-24 16:32:48 -0500

    Diseño jospeh terminado

--- app/src/main/java/pe/edu/idat/ec02_grupal/Ejercicio02.kt
+++ app/src/main/java/pe/edu/idat/ec02_grupal/Ejercicio02.kt
@@ -2,10 +2,13 @@ package pe.edu.idat.ec02_grupal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pe.edu.idat.ec02_grupal.databinding.ActivityEjercicio02Binding
 
 class Ejercicio02 : AppCompatActivity() {
+    private lateinit var binding: ActivityEjercicio02Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ejercicio02)
+        binding = ActivityEjercicio02Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
